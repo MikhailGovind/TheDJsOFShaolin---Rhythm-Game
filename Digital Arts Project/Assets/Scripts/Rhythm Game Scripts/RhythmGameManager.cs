8b6e2f3c9c8de677ce66c25bae4deefa534bd5d6
Mikhail Govind <27miks12@gmail.com>
2024-10-23 22:49:25 +0200

    Added song choice system

--- Digital Arts Project/Assets/Scripts/Rhythm Game Scripts/RhythmGameManager.cs
+++ Digital Arts Project/Assets/Scripts/Rhythm Game Scripts/RhythmGameManager.cs
@@ -1,3 +1,4 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,6 +6,8 @@ using UnityEngine.UI;
 
 public class RhythmGameManager : MonoBehaviour
 {
+    #region Variables
+
     [Header("Misc")]
     public AudioSource music;
     public bool startPlaying;
@@ -13,6 +16,12 @@ public class RhythmGameManager : MonoBehaviour
     public KeyCode scratchKeyCode;
 
     public static RhythmGameManager instance;
+
+    public bool gameState1; //pick records
+    public bool gamestate2;
+
+    [Header("Gamestate 2")]
+    public GameObject eqKnobsButtons, leftNoteHolder, rightNoteHolder, pnlOverlay, pnlCrossfader; 
 
     [Header("Score")]
     public int currentScore;
@@ -32,6 +41,48 @@ public class RhythmGameManager : MonoBehaviour
     public GameObject multiplierUpEffect, multiplierDownEffect;
 
     public bool multiplierEnum;
+
+    [Header("Records")]
+    //left record
+    public GameObject objLeftRecord;
+    public int left_songNumber;
+    public int left_maxSongNumber;
+    public string left_recordTitle;
+    public string left_artistName;
+    public int left_bpm;
+
+    public Image img_leftRecord;
+
+    public KeyCode left_SongNext;
+
+    public Text txt_left_recordTitle;
+    public Text txt_left_artistName;
+    public Text txt_left_bpm;
+
+    public Animator lp1Animator;
+
+    //right record
+    public int right_songNumber;
+    public int right_maxSongNumber;
+    public string right_recordTitle;
+    public string right_artistName;
+    public int right_bpm;
+
+    public Image img_rightRecord;
+
+    public KeyCode right_SongNext;
+
+    public Text txt_right_recordTitle;
+    public Text txt_right_artistName;
+    public Text txt_right_bpm;
+
+    //both records
+    public GameObject objMatchingBPMs;
+    public bool matchingBPMs;
+
+    //song sprites
+    public Sprite sprite_loveSong28;
+    public Sprite sprite_typeShi;
 
     [Header("Results")]
     public float totalNotes;
@@ -63,6 +114,8 @@ public class RhythmGameManager : MonoBehaviour
     public float outOfZone;
     public bool crossfaderEnum;
 
+    #endregion
+
     void Start()
     {
         instance = this;
@@ -87,13 +140,27 @@ public class RhythmGameManager : MonoBehaviour
         currentCrossfaderMultiplier = 1;
 
         crossfaderEnum = true;
+
+        left_songNumber = 1;
+        right_songNumber = 2;
+
+        matchingBPMs = true;
+
+        gameState1 = true;
+        gamestate2 = false;
+
+        eqKnobsButtons.SetActive(false);
+        leftNoteHolder.SetActive(false);
+        rightNoteHolder.SetActive(false);
+        pnlOverlay.SetActive(false);
+        pnlCrossfader.SetActive(false);
     }
 
     void Update()
     {
         #region Start music
 
-        if (!startPlaying)
+        if (!startPlaying && gamestate2)
         {
             if (Input.anyKeyDown)
             {
@@ -102,7 +169,17 @@ public class RhythmGameManager : MonoBehaviour
                 rightBeatScroller.hasStarted = true;
 
                 music.Play();
+                lp1Animator.SetTrigger("LP1_Spin");
             }
+        }
+
+        if (gamestate2)
+        {
+            eqKnobsButtons.SetActive(true);
+            leftNoteHolder.SetActive(true);
+            rightNoteHolder.SetActive(true);
+            pnlOverlay.SetActive(true);
+            pnlCrossfader.SetActive(true);
         }
 
         #endregion
@@ -189,6 +266,108 @@ public class RhythmGameManager : MonoBehaviour
 
         #endregion
 
+        #region Records
+
+        txt_left_recordTitle.text = left_recordTitle.ToString();
+        txt_left_artistName.text = left_artistName.ToString();
+        txt_left_bpm.text = "BPM: " + left_bpm;
+
+        if (gameState1)
+        {
+            if (Input.GetKeyDown(left_SongNext))
+            {
+                if (left_songNumber == (right_songNumber - 1))
+                {
+                    left_songNumber = left_songNumber + 2;
+                }
+                else
+                {
+                    left_songNumber++;
+                }
+
+                StartCoroutine(leftSongTextUp());
+
+                matchingBPMs = false;
+
+                if (left_songNumber == left_maxSongNumber + 1)
+                {
+                    left_songNumber = 1;
+                }
+
+                lp1Animator.SetTrigger("LP1_placedOn");
+            }
+        }
+
+        if (left_songNumber == 1)
+        {
+            left_recordTitle = "Love Song 28";
+            left_artistName = "Jullian Gomes";
+            left_bpm = 120;
+            img_leftRecord.sprite = sprite_loveSong28;
+        }
+
+        if (left_songNumber == 2)
+        {
+            left_recordTitle = "Type Shi";
+            left_artistName = "I";
+            left_bpm = 80;
+            img_leftRecord.sprite = sprite_typeShi;
+        }
+
+        txt_right_recordTitle.text = right_recordTitle.ToString();
+        txt_right_artistName.text = right_artistName.ToString();
+        txt_right_bpm.text = "BPM: " + right_bpm;
+
+        if (gameState1)
+        {
+            if (Input.GetKeyDown(right_SongNext))
+            {
+                if (right_songNumber == (left_songNumber - 1))
+                {
+                    right_songNumber = right_songNumber + 2;
+                }
+                else
+                {
+                    right_songNumber++;
+                }
+
+                StartCoroutine(rightSongTextUp());
+
+                matchingBPMs = false;
+
+                if (right_songNumber == right_maxSongNumber + 1)
+                {
+                    right_songNumber = 1;
+                }
+            }
+        }
+
+        if (right_songNumber == 1)
+        {
+            right_recordTitle = "Love Song 28";
+            right_artistName = "Jullian Gomes";
+            right_bpm = 120;
+            img_rightRecord.sprite = sprite_loveSong28;
+        }
+
+        if (right_songNumber == 2)
+        {
+            right_recordTitle = "Type Shi";
+            right_artistName = "I";
+            right_bpm = 80;
+            img_rightRecord.sprite = sprite_typeShi;
+        }
+
+        if (!matchingBPMs)
+        {
+            if (left_songNumber == right_songNumber)
+            {
+                StartCoroutine(matchingBPMS());
+            }
+        }
+
+        #endregion
+
         #region Scratch
 
         if (Input.GetKeyDown(scratchKeyCode))
@@ -229,6 +408,48 @@ public class RhythmGameManager : MonoBehaviour
 
         //currentScore += scorePerNote * currentMultiplier;
         txtScore.text = currentScore.ToString();
+    }
+
+    #region Text Coroutines
+
+    public IEnumerator leftSongTextUp()
+    {
+        CameraShake.Shake(duration: 0.1f, strength: 0.1f);
+
+        txt_left_bpm.fontSize = 85;
+        txt_left_bpm.color = Color.cyan;
+
+        yield return new WaitForSeconds(0.5f);
+
+        txt_left_bpm.fontSize = 75;
+        txt_left_bpm.color = Color.cyan;
+    }
+
+    public IEnumerator rightSongTextUp()
+    {
+        CameraShake.Shake(duration: 0.1f, strength: 0.1f);
+
+        txt_right_bpm.fontSize = 85;
+        txt_right_bpm.color = Color.cyan;
+
+        yield return new WaitForSeconds(0.5f);
+
+        txt_right_bpm.fontSize = 75;
+        txt_right_bpm.color = Color.cyan;
+    }
+
+    public IEnumerator matchingBPMS()
+    {
+        Instantiate(objMatchingBPMs, txt_left_bpm.transform.position, txt_left_bpm.transform.rotation);
+        Instantiate(objMatchingBPMs, txt_right_bpm.transform.position, txt_right_bpm.transform.rotation);
+
+        //CameraShake.Shake(duration: 0.2f, strength: 0.3f);
+
+        yield return new WaitForSeconds(0.25f);
+
+        matchingBPMs = true;
+        gameState1 = false;
+        gamestate2 = true;
     }
 
     public IEnumerator scoreTextUp()
@@ -306,6 +527,10 @@ public class RhythmGameManager : MonoBehaviour
         crossfaderEnum = true;
     }
 
+    #endregion
+
+    #region Note Hits
+
     public void normalHit()
     {
         currentScore += scorePerNote * currentMultiplier * currentCrossfaderMultiplier;
@@ -351,4 +576,6 @@ public class RhythmGameManager : MonoBehaviour
 
         missedNotes++;
     }
+
+    #endregion
 }
